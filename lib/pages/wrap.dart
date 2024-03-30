fdecf92311a7ced63890f5281a80fb22854caf2a
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-03-30 08:03:31 +0530

    overflow the the other widget in row and column widget thie a big problem in make ui design.

---
+++ lib/pages/wrap.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class WrapPage extends StatelessWidget {
+  const WrapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.grey.shade300,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.yellow,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red.shade400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
