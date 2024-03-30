9becc0f7ddcadfeb78629fc9307b2c356ea47587
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-03-30 08:27:30 +0530

    add spacing propertity

--- lib/pages/wrap.dart
+++ lib/pages/wrap.dart
@@ -12,79 +12,54 @@ class WrapPage extends StatelessWidget {
           child: Wrap(
             // wrap widget is a resolve the overflow condition in row & column widget
             // this widget make help build gallry patter in just five second.
-            direction: Axis.vertical,
+            direction: Axis.horizontal,
+            spacing: 8.0, //space of column
+            runSpacing: 10.0, //space of row
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red.shade400,
-                ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red.shade400,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red.shade400,
-                ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red.shade400,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 200,
-                  width: 300,
-                  color: Colors.red.shade400,
-                ),
+              Container(
+                height: 200,
+                width: 300,
+                color: Colors.red.shade400,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 150,
-                  width: 200,
-                  color: Colors.red.shade400,
-                ),
+              Container(
+                height: 150,
+                width: 200,
+                color: Colors.red.shade400,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 200,
-                  width: 150,
-                  color: Colors.yellow,
-                ),
+              Container(
+                height: 200,
+                width: 150,
+                color: Colors.yellow,
               ),
             ],
           ),
