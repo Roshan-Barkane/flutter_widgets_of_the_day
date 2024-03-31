57796b405b1dd18da926061ad48d45fdf73a5a41
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-03-31 08:07:49 +0530

    perfrom aminmation with color ,size with elevatedbutton

--- lib/pages/animationcontainer.dart
+++ lib/pages/animationcontainer.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
 
-class Animation_Container extends StatelessWidget {
+class Animation_Container extends StatefulWidget {
   const Animation_Container({super.key});
 
+  @override
+  State<Animation_Container> createState() => _Animation_ContainerState();
+}
+
+class _Animation_ContainerState extends State<Animation_Container> {
+  var _size = 200.0;
+  var _Takecolor = 0xFF00BB00;
+  var _TextColor = 0xFF000000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          AnimatedContainer(
-            duration: Duration(seconds: 2),
-            height: 200,
-            width: 200,
-            color: Color(
-              0xFF00BB00,
+          Center(
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 4),
+              height: _size,
+              width: _size,
+              color: Color(
+                _Takecolor,
+              ),
+              child: Center(
+                  child: Text(
+                "Animation Controller",
+                style: TextStyle(color: Color(_TextColor)),
+              )),
             ),
-            child: Text("Animation Controller"),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                _size = 300;
+                _Takecolor = 0xFFaa00bb;
+                _TextColor = 0xFFFFFFFF;
+              });
+            },
             child: Text("Action"),
           ),
         ],
