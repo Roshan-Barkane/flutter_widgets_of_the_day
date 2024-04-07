f3185e99e380066fc0cbfa470be10e0c20cb085c
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-07 08:52:22 +0530

    final the futurebuider widget

--- lib/pages/futurebuilder.dart
+++ lib/pages/futurebuilder.dart
@@ -8,11 +8,20 @@ class FutureBuilderPage extends StatefulWidget {
 }
 
 class _FutureBuilderPageState extends State<FutureBuilderPage> {
+  // simple templete of future function taken the some time then return the integer value / somethings
   Future<int> futureFunction() async {
     await Future.delayed(
-      Duration(seconds: 2),
+      const Duration(seconds: 2),
     );
     return 0;
+  }
+
+  // this function return the current time and date the used fatch the date api and database
+  Future<DateTime> futureFunctionaip() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    return DateTime.now();
   }
 
   @override
@@ -24,14 +33,27 @@ class _FutureBuilderPageState extends State<FutureBuilderPage> {
           children: [
             // this function is used futureBuilder in work on api intrication handle the situation
             FutureBuilder(
-              future: futureFunction(),
+              future: futureFunctionaip(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
+                  return const CircularProgressIndicator();
+                } else if (snapshot.connectionState == ConnectionState.done ||
+                    snapshot.connectionState == ConnectionState.active) {
+                  if (snapshot.hasError) {
+                    return Text(
+                      snapshot.error.toString(),
+                    );
+                  } else if (snapshot.hasData) {
+                    return Text(
+                      snapshot.data.toString(),
+                      style: const TextStyle(fontSize: 25),
+                    );
+                  } else {
+                    return Text("Some thinks is wrong");
+                  }
                 } else {
                   return Text(
-                    snapshot.data.toString(),
-                    style: TextStyle(fontSize: 25),
+                    snapshot.connectionState.toString(),
                   );
                 }
               },
