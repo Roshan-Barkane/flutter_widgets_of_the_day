4be9f61784b0e81996cb6c71c3489e6e15172856
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-08 18:05:03 +0530

    use the tickerProviderStateMixin class and create the animationController and amimation object and pass the duration , repeat(), curve etc.

---
+++ lib/pages/fadetrasition.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FaceTransistion extends StatefulWidget {
+  const FaceTransistion({super.key});
+
+  @override
+  State<FaceTransistion> createState() => _FaceTransistionState();
+}
+
+class _FaceTransistionState extends State<FaceTransistion>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: Duration(seconds: 2),
+  )..repeat(reverse: true);
+
+  late final Animation<double> _animation =
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
