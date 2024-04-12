b70ea225b0ee37494fc527333d53d2348432dc20
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-12 19:20:52 +0530

    make a dart file relative table

---
+++ lib/pages/table.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Table_Widget extends StatefulWidget {
+  const Table_Widget({super.key});
+
+  @override
+  State<Table_Widget> createState() => _Table_WidgetState();
+}
+
+class _Table_WidgetState extends State<Table_Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
