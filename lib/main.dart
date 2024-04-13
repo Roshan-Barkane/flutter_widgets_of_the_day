01ef5f99ae5d8b6b917942eca8981dced03a3cc4
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-13 07:21:49 +0530

    Table done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_widgets_of_the_day/pages/fadetrasition.dart';
 import 'package:flutter_widgets_of_the_day/pages/floatactionbutton.dart';
 import 'package:flutter_widgets_of_the_day/pages/pageview.dart';
+import 'package:flutter_widgets_of_the_day/pages/table.dart';
 //import 'package:flutter_widgets_of_the_day/pages/futurebuilder.dart';
 //import 'package:flutter_widgets_of_the_day/pages/animationcontainer.dart';
 //import 'package:flutter_widgets_of_the_day/pages/expanded.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Page_View(),
+      home: const Table_Widget(),
     );
   }
 }
--- lib/pages/table.dart
+++ lib/pages/table.dart
@@ -10,6 +10,102 @@ class Table_Widget extends StatefulWidget {
 class _Table_WidgetState extends State<Table_Widget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Table(
+          border: TableBorder.all(width: 2),
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: const [
+            TableRow(
+              children: [
+                Center(
+                  child: Text(
+                    "OS",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Icon(
+                  Icons.window,
+                  color: Colors.blue,
+                ),
+                Icon(
+                  Icons.android,
+                  color: Colors.green,
+                ),
+                Icon(
+                  Icons.apple,
+                  color: Colors.purple,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Center(
+                  child: Text(
+                    "Roshan",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+              ],
+            ),
+            TableRow(
+              children: [
+                Center(
+                  child: Text(
+                    "os",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+              ],
+            ),
+            TableRow(
+              children: [
+                Center(
+                  child: Text(
+                    "os",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+              ],
+            ),
+            TableRow(
+              children: [
+                Center(
+                  child: Text(
+                    "os",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+              ],
+            ),
+            TableRow(
+              children: [
+                Center(
+                  child: Text(
+                    "os",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+                Icon(Icons.access_alarm),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
