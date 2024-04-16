17ccf9d5fc3057e6581c52cf4aa149898167aa25
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-16 08:49:34 +0530

    used sliverList

--- lib/pages/customscollview.dart
+++ lib/pages/customscollview.dart
@@ -8,23 +8,53 @@ class CustomScrollViewWidget extends StatefulWidget {
 }
 
 class _CustomScrollViewWidgetState extends State<CustomScrollViewWidget> {
+  int count = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          const SliverAppBar(
-            backgroundColor: Colors.black,
-            title: Text(
+          SliverAppBar(
+            centerTitle: true,
+            expandedHeight: 350.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                "assets/p1.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            /*title: const Text(
               "Custom Scroll View ",
-              style: TextStyle(color: Colors.white, fontSize: 30),
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),*/
+
+            leading: Container(
+              width: 40,
+              margin: const EdgeInsets.only(left: 10),
+              decoration: const BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+              ),
+              child: const Icon(
+                Icons.arrow_left_rounded,
+                size: 50,
+                color: Colors.white,
+              ),
             ),
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate((context, index) {
-              return ListTile(
-                title: Text("Indes $index"),
-              );
+              return count != 10
+                  ? ListTile(
+                      title: Text("Indes $index"),
+                    )
+                  : Container();
             }),
           ),
         ],
