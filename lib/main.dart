65e3dbec58df327e1c1876a39850b862db951ed3
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-18 15:39:06 +0530

    ClipRRect done !

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets_of_the_day/pages/customscollview.dart';
-import 'package:flutter_widgets_of_the_day/pages/stream_builder.dart';
+import 'package:flutter_widgets_of_the_day/pages/cliprrect.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Stream_Builder(),
+      home: const ClipRRectWidget(),
     );
   }
 }
---
+++ lib/pages/cliprrect.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ClipRRectWidget extends StatefulWidget {
+  const ClipRRectWidget({super.key});
+
+  @override
+  State<ClipRRectWidget> createState() => _ClipRRectWidgetState();
+}
+
+class _ClipRRectWidgetState extends State<ClipRRectWidget> {
+  /*mostly programmer is used make the radius in image */
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 130,
+        backgroundColor: Colors.purple,
+        title: const Text(
+          "ClipRRect Widget",
+          style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1),
+        ),
+      ),
+      body: Center(
+        child: Card(
+          color: Colors.yellow,
+          child: ClipRRect(
+            child: Image.asset("assets/p1.jpg"),
+            borderRadius: BorderRadius.all(
+              Radius.circular(190),
+            ),
+            clipBehavior: Clip.hardEdge,
+          ),
+        ),
+      ),
+    );
+  }
+}
