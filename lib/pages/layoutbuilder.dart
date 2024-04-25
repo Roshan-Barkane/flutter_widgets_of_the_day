942627a1e7c841b613a2e4d6de39edfb93c35317
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-25 22:03:59 +0530

    create class layoutBuilder

---
+++ lib/pages/layoutbuilder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LayoutBuilderWidget extends StatefulWidget {
+  const LayoutBuilderWidget({super.key});
+
+  @override
+  State<LayoutBuilderWidget> createState() => _LayoutBuilderWidgetState();
+}
+
+class _LayoutBuilderWidgetState extends State<LayoutBuilderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
