2c4a7093111da542d76b58c46009b814bffca3b6
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-26 19:28:40 +0530

    write the simple code

--- lib/pages/absorbpointer.dart
+++ lib/pages/absorbpointer.dart
@@ -13,9 +13,41 @@ class _AbsorbPinterWidgetState extends State<AbsorbPinter_Widget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey,
         centerTitle: true,
         title: Text(
           widget.title.toUpperCase(),
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 7),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: 400,
+              child: const Card(
+                elevation: 10,
+                color: Colors.blue,
+                shape: BeveledRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 1),
+                ),
+              ),
+            ),
+            Container(
+              height: 150,
+              width: 300,
+              child: const Card(
+                elevation: 10,
+                color: Colors.green,
+                shape: BeveledRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 1),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
