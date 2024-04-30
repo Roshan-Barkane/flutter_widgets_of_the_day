50673a4c0eadb1f2ac8fcf12bf61b4156a896ddf
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-04-30 08:40:57 +0530

    used tanslate property in tansform widget

--- lib/pages/tansform.dart
+++ lib/pages/tansform.dart
@@ -21,9 +21,10 @@ class _TansformWidgetState extends State<TansformWidget> {
                 if +ve value you roted by unticlockwise 
                */
               angle: 1,
-              /* origin takes offset its have to paramiter 
-                x-direction and y-direction */
+              /* origin takes offset its have to paramiter means change the position
+                 x-direction [-] and y-direction [|]*/
               origin: const Offset(100, 100),
+              // offset not depand center
               child: Container(
                 height: 200,
                 width: 200,
@@ -31,7 +32,21 @@ class _TansformWidgetState extends State<TansformWidget> {
               ),
             ),
             Transform.scale(
-              scale: 0.2,
+              //scale are increase height and widget
+              scale: 0.50,
+              origin: const Offset(500, 100),
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.blue,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            /* tanslate propety is not depand origin value its take offset set the position x and y direction */
+            Transform.translate(
+              offset: const Offset(100, 10),
               child: Container(
                 height: 200,
                 width: 200,
