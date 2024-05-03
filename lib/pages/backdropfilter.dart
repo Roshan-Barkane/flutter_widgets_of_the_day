35b65137559eeec6d550340466664782a0ce2234
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-03 13:55:11 +0530

    make a content in Container

--- lib/pages/backdropfilter.dart
+++ lib/pages/backdropfilter.dart
@@ -12,51 +12,61 @@ class BackDropFilterWidget extends StatefulWidget {
 class _BackDropFilterWidgetState extends State<BackDropFilterWidget> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Image.asset(
-            "assets/p2.jpg",
-            fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              "assets/p2.jpg",
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        BackdropFilter(
-          filter: ImageFilter.blur(
-            sigmaX: 4,
-            sigmaY: 4,
-            tileMode: TileMode.clamp,
-          ),
-          child: Center(
-            child: Container(
-              height: 250,
-              width: 350,
-              decoration: BoxDecoration(
-                color: Colors.blue.shade50,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Column(
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 20,
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 4,
+              sigmaY: 4,
+              tileMode: TileMode.clamp,
+            ),
+            child: Center(
+              child: Container(
+                height: 250,
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade50,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 10),
+                      height: 50,
+                      child: const Text(
+                        "data",
+                        style: TextStyle(fontSize: 40),
                       ),
-                      Text(
-                        "Name of the Person",
-                        style: TextStyle(
-                          fontSize: 20,
+                    ),
+                    const Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
                         ),
-                      ),
-                      Icon(Icons.visibility),
-                    ],
-                  )
-                ],
+                        Text(
+                          "Name of the Person",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        Icon(Icons.visibility),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
