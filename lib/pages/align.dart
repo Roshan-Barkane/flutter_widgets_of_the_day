38ab578d1ec6b2ea0bb4820e3be3a16e67b9b94b
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-05 23:22:21 +0530

    used height Factor and widget Factor

--- lib/pages/align.dart
+++ lib/pages/align.dart
@@ -8,14 +8,15 @@ class AlignWidget extends StatelessWidget {
     return Scaffold(
       body: Align(
         //alignment: Alignment(0.4, 0.6),
-        heightFactor: 1,
-
+        heightFactor: 1.5,
+        widthFactor: 1.0,
         /* Align Widget we used to align the child diff-diff position 
         propertices :-1.  alignmernt: Alignment.topLeft,topCenter,topRight,
                                             centerLeft,center,centerRight,
                                             bottonLeft, bottonCenter,bottonRight
         with x and y Alignment(0.3,0.5),
-      2. heightFactor : value interger/double 
+      2. heightFactor : value {interger/double}  multiple height with child
+      2. widgetFactor : value {interger/double}  multiple widget with child
  */
         child: Container(
           height: 200,
