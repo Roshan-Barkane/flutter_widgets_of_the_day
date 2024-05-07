11227fefa13c813d2243691374e3733b8defc7ef
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-07 16:46:41 +0530

    used positioned widget

--- lib/pages/positioned.dart
+++ lib/pages/positioned.dart
@@ -5,6 +5,37 @@ class PositionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.orange.shade100,
+          child: Stack(
+            children: [
+              Container(height: 200, width: 200, color: Colors.yellow),
+              Container(height: 200, width: 200, color: Colors.blue),
+              Positioned(
+                  bottom: 40,
+                  left: MediaQuery.of(context).size.width / 2.5,
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Icon(
+                      Icons.add,
+                      size: 40,
+                      color: Colors.orange.shade100,
+                    ),
+                  )),
+              Container(height: 200, width: 200, color: Colors.black),
+              Container(height: 200, width: 200, color: Colors.green),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
