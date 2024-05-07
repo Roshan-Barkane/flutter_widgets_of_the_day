89ba8abd325d4c01c0dcb363e0c1829f00c72de4
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-07 16:29:42 +0530

    create positioned class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets_of_the_day/pages/align.dart';
-import 'package:flutter_widgets_of_the_day/pages/backdropfilter.dart';
+import 'package:flutter_widgets_of_the_day/pages/positioned.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AlignWidget(),
+      home: const PositionWidget(),
     );
   }
 }
---
+++ lib/pages/positioned.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PositionWidget extends StatelessWidget {
+  const PositionWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
