b6d052ee73f572b6d7fc4eb3cf60152431f3b0b8
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-05-11 06:43:56 +0530

    done SixedBox Widget

--- lib/sizeboxed.dart
+++ lib/sizeboxed.dart
@@ -13,16 +13,21 @@ class SizeboxedWidget extends StatelessWidget {
           style: TextStyle(fontSize: 30, color: Colors.white),
         ),
       ),
-      /* ElevatedButton not having and size property so apply the size with sixeBoxed */
+      /* ElevatedButton not having and size property so apply the size with sixeBoxed 
+      1. SizedBod.expand is fill all the size in parents,
+      2. SizedBox.fromSize make a circle button      */
       body: Center(
-        child: SizedBox(
-          height: double.infinity,
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              "Button",
-              style: TextStyle(fontSize: 30),
+        child: Container(
+          child: SizedBox.square(
+            //size:const  Size(200, 200),
+            /* height: double.infinity,
+            width: double.infinity, */
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "Button",
+                style: TextStyle(fontSize: 30),
+              ),
             ),
           ),
         ),
